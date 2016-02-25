18be49067ddc62d5e30d42e346da3fb3a594b861
lina_a <alexis.lina@epitech.eu>
2016-02-25 20:25:00 +0100

    commit

--- src_nm/main.c
+++ src_nm/main.c
@@ -15,14 +15,60 @@
 #include <elf.h>
 #include "../include/nm.h"
 
+char                print_char_elf(Elf64_Sym sym, Elf64_Shdr *shdr)
+{
+    char            c;
+
+    if (ELF64_ST_BIND(sym.st_info) == STB_GNU_UNIQUE)
+        c = 'u';
+    else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK)
+    {
+        c ='W';
+        if (sym.st_shndx == SHN_UNDEF)
+            c = 'w';
+    }
+    else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK &&
+            ELF64_ST_BIND(sym.st_info) == STT_OBJECT)
+    {
+        c = 'V';
+        if (sym.st_shndx == SHN_UNDEF)
+            c = 'v';
+    }
+    else if (sym.st_shndx == SHN_UNDEF)
+        c = 'U';
+    else if (sym.st_shndx == SHN_ABS)
+        c = 'A';
+    else if (sym.st_shndx == SHN_COMMON)
+        c = 'C';
+    else if (shdr[sym.st_shndx].sh_type == SHT_NOBITS
+             && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
+        c = 'B';
+    else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
+             && shdr[sym.st_shndx].sh_flags == SHF_ALLOC)
+        c = 'R';
+    else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
+             && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
+        c = 'D';
+    else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
+             && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
+        c = 'T';
+    else if (shdr[sym.st_shndx].sh_type == SHT_DYNAMIC)
+        c = 'D';
+    else
+        c = '?';
+    if (ELF64_ST_BIND(sym.st_info) == STB_LOCAL && c != '?')
+        c += 32;
+    return c;
+}
+
 void                print_sec_and_sym(Elf64_Sym sym, void *data, Elf64_Shdr *shdr)
 {
     if (sym.st_name != 0)
     {
-        if (sym.st_value != 0)
-            printf("%016x %s\n", (unsigned int) sym.st_value, (char *) (data + shdr->sh_offset + sym.st_name));
-        else
-            printf("%16s %s\n", " ", ((char *)(data + shdr->sh_offset + sym.st_name)));
+        if (sym.st_value != 0 && sym.st_info != STT_FILE)
+            printf("%016x %c %s\n", (unsigned int) sym.st_value, print_char_elf(sym, shdr),(char *) (data + shdr->sh_offset + sym.st_name));
+        else if (sym.st_info != STT_FILE)
+            printf("%16s %c %s\n", " ", print_char_elf(sym, shdr),((char *)(data + shdr->sh_offset + sym.st_name)));
     }
 }
 
@@ -49,7 +95,7 @@ char                *my_nm(void *data, Elf64_Ehdr *elf)
 }
 
 
-void        my_nm_32()
+void        my_nm_32(void *data, char *name_file)
 {
 
 }
@@ -71,6 +117,6 @@ int   main(int ac, char **av)
     }
   }
   else
-      dprintf(STDERR_FILENO,"Usage: ./my_nm <binary>");
+      dprintf(STDERR_FILENO,"Usage: ./my_nm <binary>\n");
   return (0);
 }
