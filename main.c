ee202abd5d3fd0f822d89fa9cbaa27f2833fd788
lina_a <alexis.lina@epitech.eu>
2016-02-27 17:35:04 +0100

    commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in
+**
+** Made by lina_a
+** Login   <lina_a@epitech.net>
+**
+** Started on  Sat Feb 27 17:32:47 2016 lina_a
+** Last update Sat Feb 27 17:32:47 2016 lina_a
+*/
+
+int	main(void)
+{
+  return (0);
+}
